ab27b82d832409a2265337af493b152a892bbe53
Asteriuz <gutocebola@gmail.com>
2024-12-02 18:49:52 -0300

    Started Viva Plena Site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,29 @@
 import type { Metadata } from "next";
+import { Poppins, Bebas_Neue } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["400", "700"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bebas",
+  weight: "400",
+});
+
+const gotham = localFont({
+  src: [
+    { path: "./fonts/Gotham-Font/Gotham-Black.otf", weight: "700" },
+    { path: "./fonts/Gotham-Font/GothamBook.ttf", weight: "400" },
+  ],
+  display: "swap",
+  variable: "--font-gotham",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +37,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} ${gotham.variable} ${bebas.variable} antialiased vsc-initialized flex min-h-screen min-w-full flex-col justify-between bg-white font-body text-semiblack`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,47 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        semiblack: {
+          DEFAULT: "#222",
+        },
+        cinza: {
+          DEFAULT: "#CBCBCD",
+          border: "#e1e1e",
+          text: "#6b6b6b",
+        },
+        primary: {
+          DEFAULT: "#f5811e",
+          hover: "#d96900",
+        },
+        secondary: {
+          DEFAULT: "#293d93",
+          hover: "#1a2c6b",
+        },
+        transwhite: {
+          DEFAULT: "#ffffff75",
+        },
       },
+      // screens: {
+      //   tall: { raw: "(min-height: 800px)" },
+      // },
+    },
+    fontFamily: {
+      heading: ["var(--font-gotham)"],
+      body: ["var(--font-poppins)"],
+      category: ["var(--font-bebas)"],
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "700",
     },
   },
   plugins: [],
